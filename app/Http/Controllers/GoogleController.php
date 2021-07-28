344c2ea8a19cc26517dd62bb093ed54bcdf9adc8
Rohit sharma <Rohit.inea@gmail.com>
2021-07-28 06:43:35 -0700

    Update GoogleController.php

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -10,63 +10,34 @@ class GoogleController extends Controller
 {
     //
     public function getclient(){
+         $data=["web"=>
+        ["client_id"=>"484730560750-ll7athpjotej1ibra8on74psl5d1tqsk.apps.googleusercontent.com",
+            "project_id"=>"prime-rainfall-321012","auth_uri"=>"https://accounts.google.com/o/oauth2/auth",
+            "token_uri"=>"https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url"=>"https://www.googleapis.com/oauth2/v1/certs",
+            "client_secret"=>"S1SNw_XGTyUkm18cN3qeyBG8",
+            "redirect_uris"=>["http://localhost:8000/login/google/callback"]]];
         $client=new Google_client();
         $client->setApplicationName('Google Sheets API PHP Quickstart');
         $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
-        $client->setAuthConfig('https://github.com/Rohitsharma123456/gapp/blob/719d4fed0f96f38317bc35cdadd3165ca8fc7e75/app/Http/Controllers/credentials.json');
+        $client->setAuthConfig($data);
         $client->setRedirectUri('https://immense-castle-61136.herokuapp.com/login/google/callback');
         $client->setAccessType('online');
         $client->setApprovalPrompt('consent');
         $client->setPrompt('select_account consent');
         $tokenPath = 'https://github.com/Rohitsharma123456/gapp/blob/719d4fed0f96f38317bc35cdadd3165ca8fc7e75/app/Http/Controllers/token.json';
-        $reftokenPath = 'app/Http/Controllers/reftoken.json';
-        if (file_exists($tokenPath)) {
-        $accessToken = json_decode(file_get_contents($tokenPath), true);
-        
+       
+       
+        $accessToken="ya29.a0ARrdaM-SL5ayVl6vHpLJoaaic9PlGejPbs6sdir2xxpuu5aeu8lkoDE7POZ6P_AbuOVHgaZEF1vHOcOqWvfKxXPzywDa9iABNQZC5_aKBUYbehiays1n7ixwumQJh6XEl6W8I93q1iA3O4ZTwYQhUnhTztiz3A","expires_in":3599,"scope":"https:\/\/www.googleapis.com\/auth\/spreadsheets.readonly","token_type":"Bearer","created":1627478149,"refresh_token":"1\/\/0gqKM3KNVhQpfCgYIARAAGBASNwF-L9Irvx8o4bPJ-E3rCAo14BthL6JIFJtRNhpXZgfw1rBXOGLbMqpwTGC-JEXjGSoEqT9S_aQ";
         $client->setAccessToken($accessToken);
-    }
    
     if ($client->isAccessTokenExpired()) {
         if($client->getRefreshToken()){
         $accessToken=$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
         $client->setAccessToken($accessToken);
-        if (!file_exists(dirname($reftokenPath))) {
-            mkdir(dirname($tokenPath), 0700, true);
+        
         }
-        file_put_contents($tokenPath, json_encode($accessToken));
         }
-        else{
-            if (file_exists($reftokenPath)) {
-                $refToken = json_decode(file_get_contents($reftokenPath), true);
-                $accessToken=$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-             $client->setAccessToken($accessToken);
-                } 
-                else {
-                    // Request authorization from the user.
-                    $authUrl = $client->createAuthUrl();
-                    printf("Open the following link in your browser:\n%s\n", $authUrl);
-                    print 'Enter verification code: ';
-                    $authCode = "4/0AX4XfWgzRDpNYNOu6MBXbXjAg0NvPplFfZ3GE76_ctkrMZXXmvsfWMIsE_VGFeIF-6Xoog";
-        
-                    // Exchange authorization code for an access token.
-                    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-                    $client->setAccessToken($accessToken);
-        
-                    // Check to see if there was an error.
-                    if (array_key_exists('error', $accessToken)) {
-                        throw new Exception(join(', ', $accessToken));
-                    }
-                }
-        }
-        
-        
-        
-       
-        if (!file_exists(dirname($tokenPath))) {
-            mkdir(dirname($tokenPath), 0700, true);
-        }
-        file_put_contents($tokenPath, json_encode($accessToken));
-    }
         return $client;
     }
         
